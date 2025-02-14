794dd1e4ffdcd2c9c508acec8e853f71baa88cc8
Eloyvdg <126162219+Eloyvdg@users.noreply.github.com>
2025-02-14 15:29:43 +0100

    Update Code.py

--- Hand-in 1/Code.py
+++ Hand-in 1/Code.py
@@ -1,1 +1,16 @@
+import numpy as np
 
+def factorial(n): 
+    if n == 0 or n == 1:
+        return 1
+    x = 1
+    for i in range(2, n+1):
+        x *= i    
+    return x
+
+def poisson(lamb, k):
+    return (lamb**k * np.exp(-lamb))/factorial(k)
+
+
+array_lambda = np.arange(0, 102)
+array_k = np.arange(0, 102)
