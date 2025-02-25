85f0e241a8fe19d0d3f3cb4c709d4f5064339e0e
Eloyvdg <126162219+Eloyvdg@users.noreply.github.com>
2025-02-25 11:40:52 +0100

    Update run.sh

--- Hand-in 1/run.sh
+++ Hand-in 1/run.sh
@@ -8,26 +8,12 @@ if [ ! -d "plots" ]; then
 fi
 rm -rf plots/*
 
-echo "Check if the sine movie exist"
-if [ -e sinemovie.mp4 ]; then
-  echo "Remove mp4 file"
-  rm sinemovie.mp4
-fi
-
-echo "Download image for in report..."
-if [ ! -e sine.png ]; then
-  wget home.strw.leidenuniv.nl/~daalen/Handin_files/sine.png 
-fi
-
 # Script that returns a plot
 echo "Run the first script ..."
 python3 poisson.py > poisson.txt
 
 echo "Run the second script ..."
-python3 Vandermonde.py > poisson.txt
-
-# code that makes a movie of the movie frames
-ffmpeg -framerate 25 -pattern_type glob -i "plots/snap*.png" -s:v 640x480 -c:v libx264 -profile:v high -level 4.0 -crf 10 -tune animation -preset slow -pix_fmt yuv420p -r 25 -threads 0 -f mp4 sinemovie.mp4
+python3 Vandermonde.py > vandermonde_output.txt
 
 echo "Generating the pdf"
 
