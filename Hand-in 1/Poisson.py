03e5e347715865bc20a3fcfd0ea631c2125cd056
Eloyvdg <126162219+Eloyvdg@users.noreply.github.com>
2025-02-25 11:16:28 +0100

    Update Poisson.py

--- Hand-in 1/Poisson.py
+++ Hand-in 1/Poisson.py
@@ -1,14 +1,17 @@
 import numpy as np
 
-def poisson_log(lamb, k):
-    result = k * np.log(lamb) - lamb
+def poisson(lamb, k):
+    result = k * np.log(lamb).astype(np.float32) - lamb
     log_fact = 0
     for i in range(1, k + 1):
-        log_fact += np.log(k)
-    return np.exp(result - log_fact)
+        log_fact += np.log(i).astype(np.float32)
+    return np.exp((result - log_fact).astype(np.float32))
 
 lamb = np.array([1, 5, 3, 2.6, 100, 101]).astype(np.float32)
 k = np.array([0, 10, 21, 40, 5, 200]).astype(np.int32)
-P = []
+P_k = []
 for j in range(len(k)): 
-    P += [poisson_log(lamb[j], k[j])]
+    P_k += [poisson(lamb[j], k[j])]
+
+for i in range(len(P_k)):
+    print('For ' + r'$\lambda$ =' + f' {lamb[i]:.1e} and k = {int(k[i])}, P = {P_k[i]:.6e}')
