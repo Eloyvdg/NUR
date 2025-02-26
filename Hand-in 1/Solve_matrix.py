cb86e20f55ad21a8a3d190e69f7411cd3a1cf791
Eloyvdg <126162219+Eloyvdg@users.noreply.github.com>
2025-02-26 11:39:53 +0100

    Update Solve_matrix.py

--- Hand-in 1/Solve_matrix.py
+++ Hand-in 1/Solve_matrix.py
@@ -1,27 +1,36 @@
 import numpy as np
 
 class solver():   
+    ''' Class to perform interpolation via LU decomposition. 
+    Attributes: 
+    x: list of real x values
+    y: list of real y values '''
+    
     def __init__(self, x, y): 
         self.x = x
         self.y = y
+        self.matrix = solver._crout(self.x)
     
     @staticmethod
     def _vandermonde(x): 
+        ''' Calculate the Vandermonde matrix given a list of x values'''
         size = len(x)
         matrix = np.zeros((size, size))
 
         for i in range(matrix.shape[1]):
             for j in range(matrix.shape[1]):
                 matrix[i,j] = x[i]**j
-        # self.matrix = matrix
         return matrix
     
     @staticmethod
     def _crout(x): 
-        decomposition = solver._vandermonde(x).astype(float).copy()
-        #decomposition= self.matrix.astype(float).copy()
+        ''' Calculate the LU decomposition using Crout theorem. 
+        Input: 
+        x: list of real x values '''
         
-        for j in range(decomposition.shape[1]): 
+        decomposition = solver._vandermonde(x).astype(float).copy()
+        
+        for j in range(decomposition.shape[1]): # Crout theorem
             for i in range(j+1): 
                 for k in range(i):
                     decomposition[i,j] -= decomposition[i,k] * decomposition[k,j]     
@@ -34,24 +43,29 @@ class solver():
         return decomposition
     
     def solve(self):
-        matrix = solver._crout(self.x)
-        b = self.y.copy()
-        for i in range(len(b)): 
+        ''' Solve to find c in Vc = y using the LU decomposition, and forward and backward substitution'''
+        c = self.y.copy()
+        for i in range(len(c)): # Forward substitution
             for j in range(i): 
-                b[i] -= matrix[i,j] * b[j]
-        for i in range(len(b)-1, -1, -1):
-            for j in range(i+1, len(b)):
-                b[i] -= matrix[i,j] * b[j]
+                c[i] -= self.matrix[i,j] * c[j]
+                
+        for i in range(len(c)-1, -1, -1): # Backward substitution
+            for j in range(i+1, len(c)):
+                c[i] -= self.matrix[i,j] * c[j]
             
-            b[i] /= matrix[i,i]
-        return b
+            c[i] /= self.matrix[i,i]
+        return c
     
     def iteration(self, iterations): 
-        matrix = solver._crout(self.x)
+        ''' Solve find c in Vc = y using the LU decomposition, and forward and backward substitution.
+        In this definition, it is an iterated process to try improve the result.
+        Input: 
+        iterations: the amount of iterations wanted'''
         start = solver(self.x, self.y)
-        b = start.solve()
+        b = start.solve()        
+        original = _vandermonde(self.x)
         for i in range(iterations): 
-            error = matrix @ b - self.y
+            error = original @ b - self.y
             improved = solver(self.x, error)
             b -= improved.solve()
             
