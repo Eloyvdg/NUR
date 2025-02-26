07753d7da6ea15349bb1b87487913976ce764383
Eloyvdg <126162219+Eloyvdg@users.noreply.github.com>
2025-02-26 14:15:25 +0100

    Update Vandermonde.py

--- Hand-in 1/Vandermonde.py
+++ Hand-in 1/Vandermonde.py
@@ -22,7 +22,7 @@ ya=np.zeros(len(x),dtype=np.float64) #replace!
 # Use a single LU decomposition to find the 19th order polynomial
 start = solver(x,y)
 b = start.solve()
-print('The resulting vector after solving with LU decomposition is $\textbf{c}$ ' +  f'= {b}')
+print('The resulting vector after solving with LU decomposition is \n c ' +  f'= {b}')
 
 for i in range(len(xx)): 
     yya[i] = np.dot(b, xx[i]**np.arange(0,20))
