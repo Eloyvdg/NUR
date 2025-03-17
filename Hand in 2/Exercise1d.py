93d36b714b2fe7f03954af79f589a6c5dae4f603
Eloyvdg <126162219+Eloyvdg@users.noreply.github.com>
2025-03-17 22:06:21 +0100

    Update Exercise1d.py

--- Hand in 2/Exercise1d.py
+++ Hand in 2/Exercise1d.py
@@ -8,6 +8,7 @@ b=0.25
 c=1.6
 
 def func_integrate(x, A=A, Nsat=Nsat, a=a, b=b, c=c):
+    """Function to integrate"""
     return (A*Nsat*((x/b)**(a-3))*np.exp(-(x/b)**c)) * x**2 * 4 * np.pi
 
 xmin, xmax = 10**-4, 5
@@ -23,6 +24,7 @@ def analytical_diff(x, A=A, Nsat=Nsat, a=a, b=b, c=c):
     return ((A * Nsat * b**3 * (x/b)**a * np.exp(-(x/b)**c) * (a - c * (x/b)**c - 3))* x**-4)
     
 def central_differentation(x, h, function): 
+    """Definition to calculate the central differentation"""
     if not callable(function): 
         raise ValueError
     
@@ -30,7 +32,15 @@ def central_differentation(x, h, function):
     return df
 
 def Ridder(x, h, d, e, max_iters, function):
-
+    """
+    Ridder algorithm to calculate the derivative numerically
+    x: position to differentiate
+    h: h in derivative definition
+    d: factor to decrease h with every step
+    e = target error
+    max_iters: maximum amount of iterations before stopping
+    function: function to take the derivative of
+    """
     Ridder_matrix = np.zeros([max_iters, len(x)])
     inv_d = 1/d  
     
@@ -44,12 +54,13 @@ def Ridder(x, h, d, e, max_iters, function):
         j = i+1
         Ridder_matrix[i] = (d**(2*j) * Ridder_matrix[i+1] - Ridder_matrix[i])/ (d**(2*j) - 1)
         error = np.abs(last - Ridder_matrix[i])
-        # print(error)
         last = Ridder_matrix[0].copy()
+        # Stop if the error has reached the target error
         if np.any(error < e): 
             print(f'Error is reached at iteration {i+1}')
             break
         
+        # Stop if the error starts to grow
         if np.any(error > previous_error): 
             print(f'Error is growing after iteration {i+1}')
             return Ridder_matrix[0]
@@ -57,13 +68,10 @@ def Ridder(x, h, d, e, max_iters, function):
         previous_error = error
     return Ridder_matrix[0]
 
-def test(x):
-    return x**3
-
 result_diff = Ridder(np.array([1]), 0.1, 2, 1e-10, 100, n)[0] 
 result_analytical_diff = analytical_diff(1)
 
-print(result_diff)
-print(result_analytical_diff)
+print(f'The result using Ridder algorithm is {result_diff}')
+print(f'The analytical result is {result_analytical_diff}')
 
 
