757bf590449d7eaec175220b4daaeb474cd3e4b7
Eloyvdg <126162219+Eloyvdg@users.noreply.github.com>
2025-03-17 22:06:04 +0100

    Update Exercise1abc.py

--- Hand in 2/Exercise1abc.py
+++ Hand in 2/Exercise1abc.py
@@ -11,10 +11,14 @@ b=0.25
 c=1.6
 
 def quick_sort(array): 
-    
+    """
+    Quicksort algorithm to sort an array of numbers
+    array: array with numbers to sort
+    """
     middle_idx = len(array)//2
     first, last, middle = array[0], array[-1], array[middle_idx]
     
+    # Sort the first, middle and last numbers of the array
     if first >= middle:
         array[0], array[middle_idx], array[middle_idx], array[0]
     if middle >= last: 
@@ -22,15 +26,18 @@ def quick_sort(array):
     if first >= last: 
         array[0], array[-1] = array[-1], array[0]
         
+    
     pivot = array[middle_idx]
     
     if len(array) < 3: 
+        # Array is already sorted 
         return
        
     i_flag, j_flag = False, False
     j = len(array) - 1
     i = 0
     
+    # Loop over the array from the left and right and switch if needed
     while j > i: 
         
         if i_flag == False:
@@ -56,7 +63,8 @@ def quick_sort(array):
     
     loc_pivot = np.where(array == pivot)[0][0]
 
-
+    
+    # Sort the subarrays left and right from pivot
     if len(array[:loc_pivot]) > 1: 
         quick_sort(array[:loc_pivot])
         
@@ -65,20 +73,20 @@ def quick_sort(array):
         
     return array
 
-def n(x, A=A, Nsat=Nsat, a=a, b=b, c=c):
-    return A*Nsat*((x/b)**(a-3))*np.exp(-(x/b)**c)
+ 
 
 def func_integrate(x, A=A, Nsat=Nsat, a=a, b=b, c=c):
+    """
+    Function to integrate
+    """
     return (A*Nsat*((x/b)**(a-3))*np.exp(-(x/b)**c)) * x**2 * 4 * np.pi
 
-xmin, xmax = 10**-4, 5
+xmin, xmax = 10**-4, 5 # boundaries of integral
 
 integration_init = Integration.Integrator(func_integrate, xmin, xmax)
 result, result_error = integration_init.romberg(15)
-A = 1 / result[0]
+A = 1 / result[0] # Normalization factor
 print(f'The normalization factor A is: {A}')
-
-
 
 N_generate = 10000
 
@@ -94,18 +102,20 @@ new_state2 = time.time()
 
 initial = RNG.RNG()
 
+# Rejection sampling till N_generate samples are accepted 
 while accepted_len < N_generate: 
     it += 1
     if it > 1e8: 
         break
     rand_x = np.array(initial.random_numbers(xmin, xmax))
     rand_y = np.array(initial.random_numbers(0, np.max(analytical_function)))
-    if rand_y < func_integrate(rand_x, A = A): 
+    if rand_y < func_integrate(rand_x, A = A): # Accept if smaller than analytical P(x)
         accepted_x += [rand_x]
         accepted_len += 1
     else:
         continue
 
+# Plot the results of the rejections sampling
 #21 edges of 20 bins in log-space
 edges = 10**np.linspace(np.log10(xmin), np.log10(xmax), 21)
 hist = np.histogram(accepted_x, bins=edges)[0]
@@ -120,16 +130,22 @@ ax.set(xlim=(xmin, xmax), ylim=(10**(-3), 10), yscale='log', xscale='log',
 ax.legend()
 plt.savefig('my_solution_1b.png', dpi=600)
 
+
 def fisher_yates(array, N):
+    """
+    Shuffle the array using Fisher-Yates algorithm
+    array: array to be shuffled
+    N: amount of samples to select
+    """
     for i in range(len(array))[::-1]: 
         rand_int = int(RNG.RNG().random_numbers(0,i))
         array[i], array[rand_int] = array[rand_int], array[i]
-            
     return array[:N]
 
 selection = fisher_yates(np.array(accepted_x), 100)
-sorted_selection = quick_sort(selection)
+sorted_selection = quick_sort(selection) # Sort using quicksort
 
+# Plot the results.
 #Cumulative plot of the chosen galaxies (1c)
 chosen = xmin + np.sort(np.random.rand(Nsat))*(xmax-xmin) #replace!
 fig1c, ax = plt.subplots()
