d32c252d19f7f99b8987de55f8d9b83478391cc2
Eloyvdg <126162219+Eloyvdg@users.noreply.github.com>
2025-03-17 22:06:40 +0100

    Update Exercise2a.py

--- Hand in 2/Exercise2a.py
+++ Hand in 2/Exercise2a.py
@@ -9,12 +9,14 @@ Z = 0.015
 A = 5e-10
 xi = 1e-15
 
+
 def equilibrium1(T, Z = Z, Tc = Tc, psi=psi):
     return psi*Tc*k - (0.684 - 0.0416 * np.log(T/(1e4 * Z*Z)))*T*k
 
-xmin, xmax = 1, 1e7
+xmin, xmax = 1, 1e7 # Bracket
 root_finder = rf(xmin, xmax)
 
+# Calculate the root using False position
 root_eq1 = root_finder.false_position(100, 1, 0.1, equilibrium1)
 
 
