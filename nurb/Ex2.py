6c0ba4218bb46217af7113e2d55d7eccd2657a5c
Eloyvdg <126162219+Eloyvdg@users.noreply.github.com>
2025-06-10 11:10:51 +0200

    Update and rename Exc2.py to Ex2.py

--- nurb/Ex2.py
+++ nurb/Ex2.py
@@ -1,6 +1,7 @@
 import numpy as np
 import h5py
 import matplotlib.pyplot as plt
+from Fourier import FFT, inv_FFT
 
 # Question 2: Calculating potentials
 
@@ -21,10 +22,6 @@ redshift=1.0/scale_factor-1
 rho_mean=Np*mp/L**3 #mean density in Msun/Mpc^3 (comoving, matches 3*H_0^2/(8*pi*G))
 
 # Question 2a: using Barnes-Hut [note: not actually calculating a potential, unless you do the bonus question]
-
-# TO DO: build an octree (use a class for a node, so it can also refer to child nodes; avoid using lists for anything or the memory will balloon)
-
-# Plotting the mass distribution for a slice
         
 class Node(): 
     def __init__(self, depth, child1, child2, child3, child4, child5, child6, child7, child8, 
@@ -66,17 +63,12 @@ def build_tree(array, depth, max_depth, box_center, particles, start_index, leng
     mask = np.where(mask, 1, 0)
     
     index_octant = (mask[:,0] << 2) | (mask[:,1]) << 1| (mask[:,2] << 0)
-    
-    # new_indices = np.zeros(length)
-    # ind = 0
+
     for j in range(8): 
         mask2 = index_octant == j
         new_ind = indices_check[mask2]
         len_array[j] = len(new_ind)
         octant_indices[j] = new_ind
-        # start_indices[j] = int(start_index + np.sum(len_array[:ind]))
-        # index_array[int(start_indices[j]): int(start_indices[j] + len_array[j])] = new_ind
-        # ind+=1
         
     for k in range(1,8): 
         start_indices[k] = start_indices[k-1] + len_array[k-1]
@@ -90,7 +82,6 @@ def build_tree(array, depth, max_depth, box_center, particles, start_index, leng
     new_midpoints = np.zeros((3,8))
     ind = 0
 
-    # new_indices = np.zeros(length)
     for i in range(2):
         for j in range(2): 
             for k in range(2):
@@ -111,37 +102,6 @@ def build_tree(array, depth, max_depth, box_center, particles, start_index, leng
     node = Node(depth, node1, node2, node3, node4, node5, node6, node7, node8, box_center, length, start_index, length*mp)
     
     return node
-
-
-def traverse_tree(tree, depth, x_level): 
-    
-    pixels = 2**depth
-    mapping = np.zeros((pixels, pixels))
-    center = tree.box_center
-    
-    x_middle = center[0]/pixels
-    # print(x_middle)
-    def traverse(n): 
-        if n == None: 
-            return
-        
-        if n.depth == depth: 
-
-            if n.box_center[0] == (x_middle + (2 * (x_level-1) * x_middle)):
-                y = n.box_center[1]
-                z = n.box_center[2]
-                ind_z = int((z+x_middle)/(x_middle*2) -1)
-                ind_y = int((y+x_middle)/(x_middle*2) -1)
-                mapping[ind_y, ind_z] = n.mass
-                
-                return
-        
-        if n.depth <= depth:
-            for child in [n.child1, n.child2, n.child3, n.child4, n.child5, n.child6, n.child7, n.child8]: 
-                traverse(child)
-            
-    traverse(tree)
-    return mapping
 
 def grid(tree, depth): 
     
@@ -183,10 +143,6 @@ for level in [3,5,7]: #feel free to change any of this code
     massmap=np.zeros((4,pixels,pixels),dtype=np.float32)
     for i in range(4): 
         massmap[i,:,:] = mass_grid[i,:,:]
-    
-    # TO DO: traverse the octree, fill map massmap[0,:,:] with the masses of nodes at depth 3 and x_index=x_0,
-    #        massmap[1,:,:] with the masses of nodes at depth 3 and x_index=x_1, etc; then plot these slices;
-    #        then do the same for levels 5 and 7
 
     fig, ax = plt.subplots(2,2, figsize=(10,8))
     pcm = ax[0,0].pcolormesh(np.linspace(0,L, pixels),np.linspace(0,L, pixels), massmap[0,:,:])
@@ -205,59 +161,10 @@ for level in [3,5,7]: #feel free to change any of this code
     ax[0,1].set_aspect('equal', 'box')
     ax[1,0].set_aspect('equal', 'box')
     ax[1,1].set_aspect('equal', 'box')
-    # plt.savefig(f"fig2a_level{level}.png",dpi=300)
-    plt.show()
+    plt.savefig(f"fig2a_level{level}.png",dpi=300)
     plt.close()
 
-# # Question 2b: using the FFT
-
-def FFT(array): 
-    array = np.asarray(array, dtype = np.complex64)
-    N = len(array)
-    half_N = N // 2
-    N_inv = 1/N
-    
-    if N > 2: 
-        even = FFT(array[::2])
-        odd = FFT(array[1::2])
-        array[:half_N] = even
-        array[half_N:] = odd
-
-        # array = np.concatenate((even, odd))
-        
-    comb = np.zeros(N, dtype = np.complex64)
-    
-    for k in range(0, half_N):
-        t = array[k]
-        factor = np.exp(2j*np.pi*k*N_inv) * array[k + N //2]
-        comb[k] = t + factor
-        comb[k + half_N] = t - factor
-            
-    return comb
-
-def inv_FFT(array): 
-    array = np.asarray(array, dtype = np.complex64)
-    N = len(array)
-    half_N = N // 2
-    N_inv = 1/N
-    
-    if N > 2: 
-        even = inv_FFT(array[::2])
-        odd = inv_FFT(array[1::2])
-        array[:half_N] = even
-        array[half_N:] = odd
-
-        # array = np.concatenate((even, odd))
-        
-    comb = np.zeros(N, dtype = np.complex64)
-    
-    for k in range(0, half_N):
-        t = array[k]
-        factor = np.exp(-2j*np.pi*k*N_inv) * array[k + N //2]
-        comb[k] = (t + factor) * 0.5
-        comb[k + half_N] = (t - factor) * 0.5
-            
-    return comb
+# Question 2b: using the FFT
 
 Ngrid = 128
 densgrid= grid(tree, 7) / ((L/128)**3)
@@ -279,15 +186,17 @@ for i in range(densgrid.shape[0]):
         FFT_array = FFT(potential[i,:,j])
         potential[i,:,j] = FFT_array
         
-# N = (Ngrid-1)//2 + 1
 k = np.zeros(Ngrid)
 N_half = Ngrid // 2
 k[:N_half] = np.arange(0, N_half)
-k[N_half:] = np.arange(-(N_half), 0)
-k *= 2 * np.pi / L
+k[N_half:] = np.arange(-N_half, 0)
+
+d = Ngrid/L
+k *= 2 * np.pi / L / (d * Ngrid)
 
 kx, ky, kz = np.meshgrid(k, k, k)
 k_squared = kx**2 + ky**2 + kz**2 
+
 k_squared[0,0,0] = 100
 potential = potential/k_squared
 
@@ -308,14 +217,6 @@ for i in range(densgrid.shape[0]):
    
 potential = -G/np.pi * potential.real
 
-# # TO DO: assign particle masses to densgrid, convert to density, and calculate potentials from it
-
-# # Plotting four slices of a grid
-
-# potential = np.fft.fftn(densgrid)
-# potential = -G / np.pi * np.fft.ifftn(potential/k_squared)
-# potential = potential.real
-
 fig, ax = plt.subplots(2,2, figsize=(10,8))
 pcm = ax[0,0].pcolormesh(np.linspace(0,L, Ngrid), np.linspace(0,L, Ngrid),potential[0,:,:])
 ax[0,0].set(ylabel='z (Mpc)', title=r'$\mathrm{x_0}$')
@@ -333,6 +234,5 @@ ax[0,0].set_aspect('equal', 'box')
 ax[0,1].set_aspect('equal', 'box')
 ax[1,0].set_aspect('equal', 'box')
 ax[1,1].set_aspect('equal', 'box')
-# plt.savefig("fig2b.png",dpi=300)
-plt.show()
+plt.savefig("fig2b.png",dpi=300)
 plt.close()
