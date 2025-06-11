65d94f923e181c2742c1218a99f45cfd9f36306c
Eloyvdg <126162219+Eloyvdg@users.noreply.github.com>
2025-06-11 17:24:41 +0200

    Update Ex1.py

--- nurb/Ex1.py
+++ nurb/Ex1.py
@@ -14,8 +14,9 @@ import matplotlib.pyplot as plt
 G = c.G.to(u.AU**3 * u.d**-2 * u.kg**-1)
   
 def specific_acceleration(r1, r2): 
-    # Calculates the specific acceleraition 
-    return -G.value / ( (np.sqrt(np.sum((r2 - r1)**2)) ) **3) * (r2 - r1)
+    # Calculates the specific acceleration 
+    # return -G.value / ( (np.sqrt(np.sum((r2 - r1)**2)) ) **3) * (r2 - r1)
+    return -G.value / (np.linalg.norm(r2 - r1)) * (r2 - r1)
  
 def acc_tensor(position, masses): 
     # Calculates the acceleration tensor
